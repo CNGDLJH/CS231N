527e10cfd0372246b6ae6abda59ff9314c10f050
CNGDLJH <2283581384@qq.com>
2025-11-25 00:03:39 +0800

    20251124

--- assignment1/cs231n/layers.py
+++ assignment1/cs231n/layers.py
@@ -22,7 +22,12 @@ def affine_forward(x, w, b):
     - out: output, of shape (N, M)
     - cache: (x, w, b)
     """
-    out = None
+    N = x.shape[0]
+    D = x.reshape(N,-1)
+    
+    out = np.dot(D, w) + b
+    
+    
     ###########################################################################
     # TODO: Implement the affine forward pass. Store the result in out. You   #
     # will need to reshape the input into rows.                               #
@@ -53,6 +58,18 @@ def affine_backward(dout, cache):
     """
     x, w, b = cache
     dx, dw, db = None, None, None
+    
+    N = x.shape[0]
+    D = x.reshape(N,-1)
+    
+    db = np.sum(dout, axis=0)    
+    dw = np.dot(D.T, dout)
+    dx = np.dot(dout,w.T)
+    
+    dx = dx.reshape(x.shape)
+    
+    return dx,dw,db
+    
     ###########################################################################
     # TODO: Implement the affine backward pass.                               #
     ###########################################################################
@@ -83,6 +100,9 @@ def relu_forward(x):
     #                             END OF YOUR CODE                            #
     ###########################################################################
     cache = x
+    
+    out = np.maximum(0,x)
+    
     return out, cache
 
 
@@ -98,6 +118,11 @@ def relu_backward(dout, cache):
     - dx: Gradient with respect to x
     """
     dx, x = None, cache
+    
+    masks = (x > 0).astype(float)
+    
+    dx = dout * masks
+    
     ###########################################################################
     # TODO: Implement the ReLU backward pass.                                 #
     ###########################################################################
@@ -709,7 +734,23 @@ def softmax_loss(x, y):
     - loss: Scalar giving the loss
     - dx: Gradient of the loss with respect to x
     """
+    
     loss, dx = None, None
+    
+    N = x.shape[0]
+    
+    max_x = np.max(x, axis=1, keepdims=True)
+    x_shifted = x - max_x
+    
+    exp_x = np.exp(x_shifted)
+    p = exp_x / np.sum(exp_x, axis=1, keepdims=True)
+    
+    correct_probs = p[np.arange(N) , y] + 1e-12
+    loss = np.mean(-np.log(correct_probs))
+    
+    dx = p.copy()
+    dx[np.arange(N), y] -= 1
+    dx /= N
 
     ###########################################################################
     # TODO: Copy over your solution from A1.
